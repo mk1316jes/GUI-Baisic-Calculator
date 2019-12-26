cc86b2be6536e9149471a4a965dc118272cacd54
Manas Kumar <41790202+mk1316jes@users.noreply.github.com>
2019-12-26 09:23:24 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+import tkinter as tk 
+
+
+app = tk.Tk()
+
+# Entry Widget
+top = tk.Entry(app,width=40, borderwidth=5)
+top.grid(row=0, column=0, padx=5, pady=5, columnspan=5)
+
+
+
+
+
+
+# Buttons
+
+
+
+
+
+
+
+app.mainloop()
